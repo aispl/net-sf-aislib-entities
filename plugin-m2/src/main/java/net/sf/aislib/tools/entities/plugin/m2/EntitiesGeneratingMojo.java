8970751eac85291c603b6ff94329cab82d478a36
Tomasz Pik <tompik@gmail.com>
2012-07-05 15:32:55 +0200

    implementing m2e lifecycle handling

--- plugin-m2/src/main/java/net/sf/aislib/tools/entities/plugin/m2/EntitiesGeneratingMojo.java
+++ plugin-m2/src/main/java/net/sf/aislib/tools/entities/plugin/m2/EntitiesGeneratingMojo.java
@@ -7,10 +7,12 @@ import net.sf.aislib.tools.entities.library.Processor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.sonatype.plexus.build.incremental.BuildContext;
 
 /**
  * Generate class file with constants.
@@ -19,6 +21,9 @@ import org.apache.maven.project.MavenProject;
  */
 @Mojo(defaultPhase = LifecyclePhase.GENERATE_SOURCES, name = "generate")
 public class EntitiesGeneratingMojo extends AbstractMojo {
+
+  @Component
+  private BuildContext buildContext;
 
   @Parameter(defaultValue = "${project.build.directory}/generated-sources/entities")
   private File outputDirectory;
@@ -37,14 +42,17 @@ public class EntitiesGeneratingMojo extends AbstractMojo {
 
   public void execute() throws MojoExecutionException, MojoFailureException {
     try {
+      project.addCompileSourceRoot(outputDirectory.getAbsolutePath());
+      if (!buildContext.hasDelta(sourceFile)) {
+        return;
+      }
       Processor processor = new Processor();
       processor.setSource(sourceFile);
       processor.setClassName(resultName);
       processor.setPackageName(packageName);
       processor.setDestinationDirectory(outputDirectory);
       processor.process();
-
-      project.addCompileSourceRoot(outputDirectory.getAbsolutePath());
+      buildContext.refresh(outputDirectory);
     } catch (Exception e) {
       throw new MojoExecutionException(e.getMessage(), e);
     }
